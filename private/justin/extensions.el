04f03231d3e27f9daea2494aa2599466decebb27
Justin Holguin <justin.holguin@puppetlabs.com>
2015-05-28 17:48:48 -0700

    Added a bunch of my own config stuff

---
+++ private/justin/extensions.el
@@ -0,0 +1,33 @@
+;;; extensions.el --- justin Layer extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar justin-pre-extensions
+  '(
+    ;; pre extension justins go here
+    )
+  "List of all extensions to load before the packages.")
+
+(defvar justin-post-extensions
+  '(
+    ;; post extension justins go here
+    )
+  "List of all extensions to load after the packages.")
+
+;; For each extension, define a function justin/init-<extension-justin>
+;;
+;; (defun justin/init-my-extension ()
+;;   "Initialize my extension"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
